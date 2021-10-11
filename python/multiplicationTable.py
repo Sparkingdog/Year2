713ec14e1fe10e54373ee73efb5565401dfae4c0
Meowbot <kienhong2000@hotmail.com>
2021-10-11 10:40:44 +0000

    string allignment practise

---
+++ python/multiplicationTable.py
@@ -0,0 +1,11 @@
+# practise allignment and function
+def MultTable(L, R):
+    for left in L:
+        for right in R:
+            print("{:3d} x {:3d} = {:3d}".format(left, right, left*right))
+
+
+if __name__ == "__main__":
+    lRange = range(1, 10)
+    rRange = range(1, 10)
+    MultTable(lRange, rRange)
--- python/string.py
+++ python/string.py
@@ -3,3 +3,5 @@
 # \r carriage return
 str = ("\tHello\tWorld\n\tone\tuno\n\tomg")
 print(str)
+x = "Hello {0:s},Your id is = {1:x}".format("KienHong", 109062362)
+print(x)
