bf5261d5501bcd26d188ee4a072aedf32c24ce77
Meowbot <kienhong2000@hotmail.com>
2021-12-02 02:18:51 +0000

    delete tmp

--- python/tmp1.py
+++
@@ -1,6 +0,0 @@
-L = [1, 2, 3, 4, 5]
-f = list(filter(lambda s: s % 2 == 0, L))
-print(f)
-L2 = ["Apple", "pen", "Pineapple pen", "Guava"]
-f2 = list(filter(lambda s: s in {"pen"}, L2))
-print(f2)
--- python/tmp2.py
+++
@@ -1,4 +0,0 @@
-L = [1, 2, 3, 4]
-a = list(map(lambda s: s**2, L))
-b = [-i for i in L]
-print(b)
